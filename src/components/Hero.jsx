422fb19c9cabb2d544104a1b2594cabd04d227aa
Kevin04C <kevincespedes2004@gmai.com>
2022-09-11 21:58:18 -0500

    arreglando hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ export const Hero = () => {
   return (
     <header className="max-w-7xl mx-auto px-4 md:px-0 h-screen">
       <Nav />
-      <div className="lg:flex lg:items:center lg:gap-10 mt-8">
-        <div className="basis-2/4 flex flex-col justify-center">
+      <div className="md:flex md:items:center md:gap-10 mt-10">
+        <div className="basis-2/4 flex flex-col justify-center flex-shrink">
           <p className="mb-2 font-semibold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-sky-400 to-blue-600">
             ¡Hola! yo soy
           </p>
-          <h2 className="text-slate-900 font-black text-7xl sm:text-9xl pb-5 mb-5 lg:text-9xl">
+          <h2 className="text-slate-900 font-black text-7xl sm:text-9xl md:text-8xl pb-5 mb-5 lg:text-9xl">
             Kevin <br />
             Céspedes
           </h2>
@@ -32,11 +32,11 @@ export const Hero = () => {
             }}
           />
         </div>
-        <div className="basis-2/4">
+        <div className="basis-2/4 flex-shrink">
           <img
             src={ImageProgramming}
             alt="person programming"
-            className="md:w-3/5 lg:w-full block mx-auto mt-10"
+            className="w-full block mx-auto mt-10"
           />
         </div>
       </div>
