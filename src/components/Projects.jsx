a25f303d93dfdadf37a8baf6f5e505b165f7fca2
Jose Casanova <54911067+Ja-casn@users.noreply.github.com>
2022-09-22 17:54:51 -0400

    Update projects.jsx
    Modificando solo unas letritras, que tenian errores :D!

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ export const Projects = () => {
     <section className="my-42" id="proyectos">
       <h2 className="font-extrabold text-5xl mb-10 text-center">Mis Proyectos</h2>
       <p className="text-slate-600 text-xl md:text-2xl md:w-2/4 mx-auto mb-10">
-        Algunos de mis proyectos desarrolados como DESARROLADOR WEB <span className="font-bold">FRONT-END</span>,
+        Algunos de mis proyectos desarrollados como DESARROLLADOR WEB <span className="font-bold">FRONT-END</span>,
         puedes ver el proyecto terminado y el código fuente en <span className="font-bold">GitHub</span>. 
       </p>
       
