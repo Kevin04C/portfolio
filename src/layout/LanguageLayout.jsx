466aef3fb987840f5fe55d890403c6522d194aba
Kevin04C <kevincespedes2004@gmai.com>
2022-11-13 13:52:24 -0500

    remove cursor-pointer

--- src/layout/LanguageLayout.jsx
+++ src/layout/LanguageLayout.jsx
@@ -1,7 +1,7 @@
 
 export const LanguageLayout = ({children}) => {
   return (
-    <div className="w-24 mx-auto hover:cursor-pointer hover:p-4 hover:bg-gray-900 hover:rounded-md hover:fill-slate-200 transition-all hover:shadow-lg">
+    <div className="w-24 mx-auto hover:p-4 hover:bg-gray-900 hover:rounded-md hover:fill-slate-200 transition-all hover:shadow-lg">
       {children}
     </div>
   )
