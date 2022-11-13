b0fcf9b61dfadc2db58dc0d572e8c51b49a3f1e5
Kevin04C <kevincespedes2004@gmai.com>
2022-11-13 13:59:53 -0500

    add linkedin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,17 @@ export const Footer = () => {
       <div className="md:w-2/4 mx-auto text-center">
         <p className="text-xl font-extralight mb-5">Hecho con mucho 💟.</p>
         <ul className="flex gap-5 justify-center text-sm">
-          <li className="p-2 hover:bg-slate-500 md:rounded transition-all text-slate-200">
+          <li className="p-2 hover:bg-slate-700 md:rounded transition-all text-slate-200">
             <a href="https://www.instagram.com/zkevin.2004/" target="_blank">Instagram</a>
           </li>
-          <li className="p-2 hover:bg-slate-500 md:rounded transition-all text-slate-200">
+          <li className="p-2 hover:bg-slate-700 md:rounded transition-all text-slate-200">
             <a href="https://www.github.com/kevin04c" target="_blank">Github</a>
           </li>
-          <li className="p-2 hover:bg-slate-500 md:rounded transition-all text-slate-200">
-            <a href="https://www.tiktok.com/@kevin.dev" target="_blank">Tik Tok</a>
+          <li className="p-2 hover:bg-slate-700 md:rounded transition-all text-slate-200">
+            <a href="https://www.tiktok.com/@kevin.dev" target="_blank">Tiktok</a>
+          </li>
+          <li className="p-2 hover:bg-slate-700 md:rounded transition-all text-slate-200">
+            <a href="https://www.linkedin.com/in/kevin-orealy-c%C3%A9spedes-%C3%A1lvarez-943818242/" target="_blank">Linkedin</a>
           </li>
         </ul>
       </div>
