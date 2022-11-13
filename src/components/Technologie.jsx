0597164c72aa5d3714dfea89f080f0be7bea6c26
Kevin04C <kevincespedes2004@gmai.com>
2022-11-13 14:03:58 -0500

    chnage text color

--- src/components/Technologie.jsx
+++ src/components/Technologie.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Technologie = ({name}) => {
   return (
-    <span className="bg-slate-600 py-1 px-2 rounded-lg text-sm font-sembold">{name}</span>
+    <span className="bg-slate-700 text-slate-50 font-semibold py-1 px-2 rounded-lg text-sm font-sembold">{name}</span>
   )
 }
