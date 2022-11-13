c4061e1784fb2b3b02145e0a975123fc8a178177
Kevin04C <kevincespedes2004@gmai.com>
2022-11-13 14:03:29 -0500

    change project title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,9 +3,9 @@ import { CardList } from './CardList';
 export const Projects = () => {
   return (
     <section className="my-42" id="proyectos">
-      <h2 className="font-extrabold text-5xl mb-10 text-center">Mis Proyectos</h2>
+      <h2 className="font-extrabold text-5xl mb-10 text-center">Mis proyectos</h2>
       <p className="text-slate-600 text-xl md:text-2xl md:w-2/4 mx-auto mb-10">
-        Algunos de mis proyectos desarrolados como DESARROLADOR WEB <span className="font-bold">FRONT-END</span>,
+        Algunos de mis proyectos desarrolados,
         puedes ver el proyecto terminado y el código fuente en <span className="font-bold">GitHub</span>. 
       </p>
       
